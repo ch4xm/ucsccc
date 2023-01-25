b2a664a32a83c00542b4190874bacdf697816484
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-01-25 15:14:32 -0800

    added CORS

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from ucsc import ucsc
 
 from flask import Flask
 from flask_compress import Compress
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api": {"origins": "*"}})
 Compress(app)
 app.register_blueprint(ucsc)
