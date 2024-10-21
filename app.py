9214a9373e4dab674d289eaca315f3422c330f7f
Your Name <you@example.com>
2024-10-21 14:04:09 -0700

    add cache reload to main

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from ucsc import ucsc
+from ucsc import ucsc, main
 
 from flask import Flask
 from flask_compress import Compress
 from flask_cors import CORS
 
+main()
 app = Flask(__name__)
 CORS(app, resources={r"/api": {"origins": "*"}})
 Compress(app)
